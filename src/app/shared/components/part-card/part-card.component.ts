a26372256f28ad1b4258b8a9b49af78a152253dd
Iftikhar Yar Khan <iftikharyk@gmail.com>
2025-06-06 03:41:12 +0500

    added dummy data and routing

--- src/app/shared/components/part-card/part-card.component.ts
+++ src/app/shared/components/part-card/part-card.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-part-card',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './part-card.component.html',
   styleUrl: './part-card.component.css'
 })
 export class PartCardComponent {
-
+  @Input() part: any;
 }
