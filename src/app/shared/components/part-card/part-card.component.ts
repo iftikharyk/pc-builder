0792c62be65a46652a29467c7823c89d0cbe988e
Iftikhar Yar Khan <iftikharyk@gmail.com>
2025-06-06 03:09:12 +0500

    first commit

---
+++ src/app/shared/components/part-card/part-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-part-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './part-card.component.html',
+  styleUrl: './part-card.component.css'
+})
+export class PartCardComponent {
+
+}
